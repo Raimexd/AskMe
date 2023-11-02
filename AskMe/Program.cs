7604d863b5c4c9a0d4eb268ceae75649c8489f7b
212226 <212226@1220-002-22>
2023-11-02 11:06:51 +0300

    Добавьте файлы проекта.

---
+++ AskMe/Program.cs
@@ -0,0 +1,53 @@
+﻿
+Random rnd = new Random();
+int a;
+
+while (true)
+{
+
+    Console.WriteLine("Спроси что угодно!");
+    Console.ReadLine();
+
+    a = rnd.Next(6);
+
+    switch (a)
+    {
+        case 0:
+            Console.WriteLine("Да!");
+            Console.WriteLine("Нажми Enter, если не лох");
+            Console.ReadLine();
+            break;
+
+        case 1:
+            Console.WriteLine("Нет!");
+            Console.WriteLine("Нажми Enter, если не лох");
+            Console.ReadLine();
+            break;
+
+        case 2:
+            Console.WriteLine("Возможно!");
+            Console.WriteLine("Нажми Enter, если не лох");
+            Console.ReadLine();
+            break;
+
+        case 3:
+            Console.WriteLine("Я по вашему мнению должен это знать?!");
+            Console.WriteLine("Нажми Enter, если не лох");
+            Console.ReadLine();
+            break;
+
+        case 4:
+            Console.WriteLine("Скорее да, чем нет!");
+            Console.WriteLine("Нажми Enter, если не лох");
+            Console.ReadLine();
+            break;
+
+        case 5:
+            Console.WriteLine("Скорее нет, чем да!");
+            Console.WriteLine("Нажми Enter, если не лох");
+            Console.ReadLine();
+            break;
+    }
+
+    Console.Clear();
+}
